376ea19778d025b861c34ffff40de99d3b02bd4c
icokie <icokie@yandex.ru>
2020-05-16 01:26:36 +0200

    refactoring

--- src/contentScript/index.tsx
+++ src/contentScript/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { extId } from '@elena:consts'
-
 import './style.less'
+import { extId } from '../consts'
 import { PopUpApp } from './PopUpApp'
 
-let container = document.getElementById(extId)
-
-if (!container) {
-  container = document.createElement('div')
-  container.id = extId
-  document.body.appendChild(container)
-}
+const container = document.createElement('div')
+container.id = extId
+document.body.appendChild(container)
 
 ReactDom.render(<PopUpApp />, container)
