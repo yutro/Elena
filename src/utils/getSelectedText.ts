39d61ee7aa3fbcb85dc001471813e785c005e115
icokie <icokie@yandex.ru>
2020-05-16 00:22:52 +0200

    add getSelectedText helper

--- src/utils/getSelectedText.ts
+++ src/utils/getSelectedText.ts
@@ -2,4 +2,8 @@ export const getSelectedText = ({
   anchorOffset: selectionStart,
   focusOffset: selectionEnd,
   focusNode: selectedElement,
-}: Selection): string | null => selectedElement?.textContent?.slice(selectionStart, selectionEnd) ?? null
+}: Selection): string | null => {
+  const text = selectedElement?.textContent?.slice(selectionStart, selectionEnd)
+
+  return text?.length ? text : null
+}
