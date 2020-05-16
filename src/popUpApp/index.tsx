dee82b2304c2c81b538f4647412040bc1cd8ce48
icokie <icokie@yandex.ru>
2020-05-16 02:25:43 +0200

    add extension pop up section

---
+++ src/popUpApp/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import { PopUpAppContainer } from './src'
+
+ReactDom.render(<PopUpAppContainer />, document.getElementById('appRoot'))
---
+++ src/popUpApp/src/index.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from 'react'
+
+export const PopUpAppContainer: FunctionComponent = () => {
+  return <div>pop up container</div>
+}
