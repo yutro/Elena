02f894b6f513ea1d6d333cd38baed7b4b2158549
icokie <icokie@yandex.ru>
2020-05-23 15:40:39 +0200

    update tailwind purge unused css

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,9 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    ...(isProduction && {
-      'postcss-remove-unused-css': {
-        path: './src/contentScript',
-        exts: ['.tsx'],
-      },
-    }),
+    tailwindcss: {
+      purge: ['./src/**/*.html'],
+    },
+    precss: {},
   },
 }
