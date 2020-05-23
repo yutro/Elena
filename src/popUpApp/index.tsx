723f2167a4579dac34ede5ccf82863ba13d75183
icokie <icokie@yandex.ru>
2020-05-23 15:44:11 +0200

    update popUpApp

--- src/popUpApp/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import './style.less'
-
-import { PopUpAppContainer } from './src'
-
-ReactDom.render(<PopUpAppContainer />, document.getElementById('appRoot'))
--- src/popUpApp/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FunctionComponent } from 'react'
-
-export const PopUpAppContainer: FunctionComponent = () => {
-  return <div>pop up container</div>
-}
